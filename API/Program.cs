e7a71e5b95c2f4a5f186181d3f513872b7a0dfa0
Sambid Prasad Rijal <sambidrijal71@gmail.com>
2025-05-29 23:32:02 +1000

    Downgrade dotnet to 8 for CI compatible.

--- API/Program.cs
+++ API/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
 
@@ -20,12 +19,6 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseCors(opt =>
 {
